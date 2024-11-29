8ede6cf578bf60858aaf577bd3c4c8236e6cf788
Ahmet <ahmetf_y@outlook.com>
2024-11-29 22:18:30 +0300

    SubHeader component commit

---
+++ components/SubHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="q-pa-md">
+        <div class="row">
+          <div class="col bg-deep-purple-6">
+            <q-btn flat color="white" label="Giyim" class="full-width" />
+          </div>
+          <div class="col bg-teal-6">
+            <q-btn flat color="white" label="Çanta" class="full-width" />
+          </div>
+          <div class="col bg-pink-8">
+            <q-btn flat color="white" label="Ayakkabı" class="full-width" />
+          </div>
+          <div class="col bg-amber-6">
+            <q-btn flat color="white" label="Aksesuar" class="full-width" />
+          </div>
+        </div>
+      </div>
+</template>
