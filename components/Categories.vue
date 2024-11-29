fa61a68d223efec42206b9c9f1b12812c9fb71ea
Eren <89195983+Erensnc@users.noreply.github.com>
2024-11-29 23:02:15 +0300

    Categories component operations done
    kategori sayfası yapıldı.

---
+++ components/Categories.vue
@@ -0,0 +1,120 @@
+<template>
+     <q-list class="rounded-borders">
+              <q-expansion-item
+                expand-separator
+                icon="list"
+                label="KATEGORİLER"
+                caption="Kadın"
+                default-opened
+                style="font-weight: bolder"
+              >
+                <q-expansion-item
+                  :header-inset-level="1"
+                  expand-separator
+                  icon="list"
+                  label="Ayakkabı"
+                  default-opened
+                >
+                  <q-expansion-item
+                    switch-toggle-side
+                    dense-toggle
+                    label="Spor Ayakkabı"
+                    :header-inset-level="1"
+                    :content-inset-level="2"
+                  >
+                    <q-btn
+                      flat
+                      class="full-width"
+                      color="dark"
+                      label="Az Kullanılmış"
+                      style="opacity: 0.75"
+                    />
+                    <q-btn
+                      flat
+                      color="dark"
+                      class="full-width"
+                      label="Yeni & Etiketli"
+                      style="opacity: 0.75"
+                    />
+                    <q-btn
+                      flat
+                      class="full-width"
+                      color="dark"
+                      label="Yeni Gibi"
+                      style="opacity: 0.75"
+                    />
+                  </q-expansion-item>
+                </q-expansion-item>
+              </q-expansion-item>
+  
+              <!-- Lokasyonlar Listesi -->
+              <q-expansion-item
+                :content-inset-level="0.5"
+                expand-separator
+                icon="place"
+                label="LOKASYONLAR"
+                caption="İstanbul"
+                default-opened
+                style="font-weight: bolder"
+              >
+                <q-expansion-item
+                  expand-separator
+                  :content-inset-level="0.5"
+                  icon="place"
+                  label="Türkiye"
+                >
+                  <q-expansion-item label="İstanbul" :content-inset-level="0.5">
+                    <q-btn
+                      flat
+                      color="dark"
+                      label="Ümraniye"
+                      style="opacity: 0.75"
+                      class="full-width"
+                    />
+                    <q-btn
+                      flat
+                      color="dark"
+                      label="Ataşehir"
+                      style="opacity: 0.75"
+                      class="full-width"
+                    />
+                    <q-btn
+                      flat
+                      color="dark"
+                      label="Pendik"
+                      style="opacity: 0.75"
+                      class="full-width"
+                    />
+                    <q-btn
+                      flat
+                      color="dark"
+                      label="Bağcılar"
+                      style="opacity: 0.75"
+                      class="full-width"
+                    />
+                    <q-btn
+                      flat
+                      color="dark"
+                      label="Esenyurt"
+                      style="opacity: 0.75"
+                      class="full-width"
+                    />
+                    <q-btn
+                      flat
+                      color="dark"
+                      label="Yenikapı"
+                      style="opacity: 0.75"
+                      class="full-width"
+                    />
+                    <q-btn
+                      flat
+                      color="dark"
+                      label="Üsküdar"
+                      style="opacity: 0.75"
+                      class="full-width"
+                    />
+                  </q-expansion-item>
+                </q-expansion-item>
+              </q-expansion-item>
+            </q-list>
+</template>
---
+++ pages/Ayakkabi/index.vue
@@ -0,0 +1,58 @@
+<template>
+    <q-page>
+      <Header />
+  
+      <SubHeader />
+  
+      <div class="q-col">
+        <div class="kategori" style="width: 40em">
+          <div class="q-pa-md" style="width: 26em">
+            <!-- Kategori Listesi -->
+            <Categories />
+  
+            <!-- Fiyat Filtreleri -->
+            <Price />
+          </div>
+        </div>
+  
+        <!-- Ürünler -->
+        <div>
+          <div class="text-h6" style="position: relative; left: 55px">
+            Spor Ayakkabı <b>9.578 ürün</b>
+          </div>
+            <ProductCard2 />
+        </div>
+      </div>
+    </q-page>
+  </template>
+  
+  <script>
+  import { ref } from "vue";
+import Header from "~/components/Header.vue";
+  
+  export default {
+    setup() {
+      const arama = ref("");
+      const ratingModel = ref(4);
+      const items2 = ref([]);
+  
+      // Rastgele Ürün Verisi Yükleme
+      const loadItems = () => {
+        items2.value = Array.from({ length: 15 }, (_, index) => ({
+          id: index + 1,
+          avatar: `/Screenshot_${3061 + (index % 12)}.png`,
+          satici: `Satıcı ${index + 1}`,
+          rate: (Math.random() * 5).toFixed(1),
+          source: `/Screenshot_${3024 + (index % 37)}.png`,
+          info: `Ürün ${index + 1}`,
+          fiyat: (Math.random() * 500).toFixed(2),
+        }));
+      };
+  
+      loadItems();
+  
+      return { arama, ratingModel, items2 };
+    },
+  };
+  </script>
+  
